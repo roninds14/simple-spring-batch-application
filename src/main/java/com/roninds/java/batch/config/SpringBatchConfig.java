27bf275895fbdf0ec50887941d66309397094821
roninds14 <roninds14@gmail.com>
2024-07-25 16:30:47 -0300

    batch configuration initiate

---
+++ src/main/java/com/roninds/java/batch/config/SpringBatchConfig.java
@@ -0,0 +1,41 @@
+package com.roninds.java.batch.config;
+
+import com.roninds.java.batch.model.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class SpringBatchConfig {
+
+    @Bean
+    public Job job(
+            JobBuilderFactory jobBuilderFactory,
+            StepBuilderFactory stepBuilderFactory,
+            ItemReader<User> itemReader,
+            ItemProcessor<User, User> itemProcessor,
+            ItemWriter<User> itemWriter) {
+
+        Step step = stepBuilderFactory.get("ETL-file-load")
+                .<User, User>chunk(100)
+                .reader(itemReader)
+                .processor(itemProcessor)
+                .writer(itemWriter)
+                .build();
+
+        return jobBuilderFactory.get("ETL-Load")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+}
