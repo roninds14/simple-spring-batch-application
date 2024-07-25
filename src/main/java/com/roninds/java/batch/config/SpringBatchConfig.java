9636dd39463a64f3a520f62ffd8901596c5adcb4
roninds14 <roninds14@gmail.com>
2024-07-25 16:41:04 -0300

    create itemReader

--- src/main/java/com/roninds/java/batch/config/SpringBatchConfig.java
+++ src/main/java/com/roninds/java/batch/config/SpringBatchConfig.java
@@ -10,6 +10,10 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,5 +41,36 @@ public class SpringBatchConfig {
                 .incrementer(new RunIdIncrementer())
                 .start(step)
                 .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> fileItemReader(@Value("${inputFile}") Resource resource) {
+
+        FlatFileItemReader<User> flatFileItemReader = new FlatFileItemReader<>();
+
+        flatFileItemReader.setResource(resource);
+        flatFileItemReader.setName("CSV-Reader");
+        flatFileItemReader.setLinesToSkip(1);
+        flatFileItemReader.setLineMapper(lineMapper());
+
+        return flatFileItemReader;
+    }
+
+    @Bean
+    public LineMapper<User> lineMapper() {
+        DefaultLineMapper<User> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames(new String[]{"id", "name", "dept", "salary"});
+
+        BeanWrapperFieldSetMapper<User> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(User.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return defaultLineMapper;
     }
 }
