962aa614201c9edd822066b61dfc44486826a56f
roninds14 <roninds14@gmail.com>
2024-07-25 18:38:21 -0300

    creating service

--- src/main/java/com/roninds/java/batch/model/User.java
+++ src/main/java/com/roninds/java/batch/model/User.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @Entity
-@Table(name = "\"user\"")
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/java/com/roninds/java/batch/service/LoadService.java
+++ src/main/java/com/roninds/java/batch/service/LoadService.java
@@ -1,7 +1,5 @@
-package com.roninds.java.batch.controller;
+package com.roninds.java.batch.service;
 
-import com.roninds.java.batch.service.LoadService;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
@@ -14,29 +12,40 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@Getter
-@RestController
-@RequestMapping("/load")
-public class LoadController {
+@Service
+public class LoadService {
 
     @Autowired
-    LoadService loadService;
+    JobLauncher jobLauncher;
 
-    @GetMapping
+    @Autowired
+    Job job;
+
     public BatchStatus load () throws
             JobInstanceAlreadyCompleteException,
             JobExecutionAlreadyRunningException,
             JobParametersInvalidException,
             JobRestartException {
+        Map<String, JobParameter> map = new HashMap<>();
+        map.put("time", new JobParameter(System.currentTimeMillis()));
 
-        return getLoadService().load();
+        JobParameters parameters = new JobParameters(map);
+
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+
+        log.info("JobExecution: " + jobExecution.getStatus());
+
+        log.info("Batch is Running...");
+        while (jobExecution.isRunning()){
+            log.info("...");
+        }
+
+        return jobExecution.getStatus();
     }
 }
